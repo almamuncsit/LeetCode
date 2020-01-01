9ea1ab4e6cfd152236dad27e476a6a7f9a408325
Art 0f CSE <artofcse@gmail.com>
2020-01-02 00:07:39 +0600

    Solve a problem of array

---
+++ array/05 Single Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        length = len(nums)
+        i = 0
+        while i < length:
+            if i == length-1:
+                return nums[i]
+            if nums[i] != nums[i+1]:
+                return nums[i] 
+            i += 2
